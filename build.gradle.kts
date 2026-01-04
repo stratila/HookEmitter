a29d8e11d7e3367c919ec920086cdd1ece4d751e
Bohdan Stratila <googlobody@gmail.com>
2026-01-04 22:10:44 +0100

    Implement HookEmitterPlugin class

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("com.gradleup.shadow") version "9.3.0"
 }
 
 repositories {
@@ -8,4 +9,13 @@ repositories {
 
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.21.10-R0.1-SNAPSHOT")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.17.0")
+    implementation("org.apache.commons:commons-text:1.12.0")
+}
+
+tasks.shadowJar {
+    relocate(
+        "org.apache.commons.text",
+        "io.stratila.hook.emitter.libs.commons.text"
+    )
 }
--- src/main/java/io/stratila/hook/emitter/plugin/HookEmitterPlugin.java
+++ src/main/java/io/stratila/hook/emitter/plugin/HookEmitterPlugin.java
@@ -1,9 +1,82 @@
 package io.stratila.hook.emitter.plugin;
 
-import java.net.http.WebSocket.Listener;
+import java.util.Map;
+import java.util.logging.Level;
 
+import net.kyori.adventure.text.Component;
+import org.apache.commons.text.StringSubstitutor;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class HookEmitterPlugin extends JavaPlugin implements Listener{
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import io.stratila.async.http.client.AsyncHttpClient;
+import org.jetbrains.annotations.NotNull;
+
+public class HookEmitterPlugin extends JavaPlugin implements Listener {
+    @Override
+    public void onEnable() {
+        Bukkit.getPluginManager().registerEvents(this, this);
+        saveResource("config.yml", false);
+
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        sendTelegramRequest(event);
+    }
+
+    private void sendTelegramRequest(PlayerEvent event) {
+        String endpoint = getConfig().getString("eventHooks.telegramEndpoint");
+        if (endpoint == null)
+            return;
+
+        Player player = event.getPlayer();
+
+        String formattedString = buildFormattedJoinMessage(player, endpoint);
+        String jsonPayload = buildJSONJoinPayload(player, formattedString);
+
+        AsyncHttpClient.postJson(endpoint, jsonPayload).thenAccept(response -> {
+            // Switch back to main thread if interacting with Bukkit
+            Bukkit.getScheduler().runTask(this, () -> {
+                getLogger().log(Level.INFO, "HookEmitter: payload " + jsonPayload + " has been sent to " + endpoint);
+                player.sendMessage(Component.text("HookEmitter: Send request to a Telegram about " + player.getName()));
+            });
+        }).exceptionally(ex -> {
+            getLogger().log(Level.SEVERE, "Failed to call endpoint: " + endpoint, ex);
+            return null;
+        });
+    }
+
+    private String buildFormattedJoinMessage(@NotNull Player player, String endpoint) {
+        String defaultMessage = getConfig().getString("templates.defaultMessage");
+        Map<String, String> values = Map.of(
+                "player", player.getName(),
+                "endpoint", endpoint);
+
+        return StringSubstitutor.replace(defaultMessage, values);
+    }
+
+    private String buildJSONJoinPayload(@NotNull Player player, String joinMessage) {
+        ObjectMapper mapper = new ObjectMapper();
+        ObjectNode json = mapper.createObjectNode();
+        ObjectNode target = mapper.createObjectNode();
+        ObjectNode meta = mapper.createObjectNode();
+
+        target.put("name", player.getName());
+        target.put("uuid", player.getUniqueId().toString());
+        meta.put("join_message", joinMessage);
+        json.set("player", target);
+        json.set("meta", meta);
+
+        return json.toString();
+    }
 
 }
