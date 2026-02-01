5c35f395dc3764cfaf737f33af59ad98a0d600f7
Bohdan Stratila <googlobody@gmail.com>
2026-02-01 14:21:55 +0100

    Add hookemitter.join_msg.others permission

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("java")
-    id("com.gradleup.shadow") version "9.3.0"
     id("idea")
     id("com.diffplug.spotless") version "8.1.0"
 }
@@ -22,14 +21,8 @@ spotless {
 }
 
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:1.21.10-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:1.21.11-R0.1-SNAPSHOT")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.17.0")
     implementation("org.apache.commons:commons-text:1.12.0")
 }
 
-tasks.shadowJar {
-    relocate(
-        "org.apache.commons.text",
-        "io.stratila.hook.emitter.libs.commons.text"
-    )
-}
