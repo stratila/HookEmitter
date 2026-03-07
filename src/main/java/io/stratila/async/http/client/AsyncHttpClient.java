96ced2a559e6ac590d0d47b62b624169d68b9cbf
Bohdan Stratila <googlobody@gmail.com>
2026-03-07 22:36:14 +0100

    Use HTTP 1.1

--- src/main/java/io/stratila/async/http/client/AsyncHttpClient.java
+++ src/main/java/io/stratila/async/http/client/AsyncHttpClient.java
@@ -11,7 +11,12 @@ public class AsyncHttpClient {
   private static final HttpClient CLIENT = HttpClient.newHttpClient();
 
   public static CompletableFuture<String> get(String url) {
-    HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url)).GET().build();
+    HttpRequest request =
+        HttpRequest.newBuilder()
+            .uri(URI.create(url))
+            .version(HttpClient.Version.HTTP_1_1)
+            .GET()
+            .build();
 
     return CLIENT
         .sendAsync(request, HttpResponse.BodyHandlers.ofString())
@@ -22,6 +27,7 @@ public class AsyncHttpClient {
     HttpRequest request =
         HttpRequest.newBuilder()
             .uri(URI.create(url))
+            .version(HttpClient.Version.HTTP_1_1)
             .header("Content-Type", "application/json")
             .POST(HttpRequest.BodyPublishers.ofString(json))
             .build();
